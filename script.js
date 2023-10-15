c65e6a95685dc43733b6012cf5593ab48edb14aa
gabrielribeiro <gabriel.ribeiro.fernandes00@gmail.com>
2023-10-14 22:52:26 -0300

    change order value with a input

--- script.js
+++ script.js
@@ -5,34 +5,39 @@ var myList = document.getElementById("orderList")
 var nextTurn = document.getElementById("nextTurn")
 
 
+
 function addItem(){
   var text = myTextBox.value
   var iniciative = iniBox.value
 
   if ((text && iniciative) !== "") {
     var newItem = document.createElement("li")
+    var iniValue = document.createElement("input")
+
     newItem.textContent = text
     newItem.id = iniciative
     newItem.title = iniciative
+
+    iniValue.value = iniciative
+    
     myList.appendChild(newItem)
+    newItem.appendChild(iniValue)
     myTextBox.value = ""
     iniBox.value = ""
     myTextBox.focus();
   }
-  var itens = myList.children
-  for (var i = 0; i < itens.length; i++) {
-    for (var j = i + 1; j < itens.length; j++) {
-      if (parseInt(itens[i].id) < parseInt(itens[j].id)) {
-        var save = itens[i]
-        myList.removeChild(itens[i])
-        myList.appendChild(save)
-        i = 0
-        j = 1
+  sortItems()
+}
+function sortItems() {
+  var items = myList.children;
+  for (var i = 0; i < items.length; i++) {
+    for (var j = i + 1; j < items.length; j++) {
+      if (items[i].id < items[j].id) {
+        myList.insertBefore(items[j], items[i]);
       }
     }
   }
 }
-
 button.addEventListener("click", function () {
   addItem();
 })
@@ -47,8 +52,25 @@ iniBox.addEventListener("keydown", function (event) {
   }
 });
 
+myTextBox.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+      addItem();
+  }
+});
+
 nextTurn.addEventListener("click", function () {
   var lastElement = myList.lastElementChild
   myList.insertBefore(lastElement, myList.firstElementChild)
 
 })
+
+myList.addEventListener("input", function (event) {
+  var target = event.target;
+  if (target.tagName === "INPUT") {
+    var listItem = target.closest("li");
+    if (listItem) {
+      listItem.id = target.value;
+      listItem.title = target.value;
+    }
+  }
+});
