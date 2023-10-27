bd720eb47d53fd44e81d2217f8ec2fa0589cf953
Gabriel Ribeiro <144161116+gabrielribbz@users.noreply.github.com>
2023-10-27 14:36:20 -0300

    addeffectsystem

--- script.js
+++ script.js
@@ -6,13 +6,20 @@ var nextTurn = document.getElementById("nextTurn")
 var previousTurn = document.getElementById("previousTurn")
 var refresh = document.getElementById("refresh")
 var Delete = document.getElementById("delete")
+var selectElement = document.getElementById("selectElement")
+var turnNumber = document.getElementById("turnNumber")
+var diceNumber = document.getElementById("diceNumber")
+var opcoes = document.getElementById("opcoes")
 
+var whichTurnIs = 0;
+
+var block = 0;
 
 function addItem(){
   var text = myTextBox.value
   var iniciative = iniBox.value
 
-  if (((text && iniciative) !== "") && (Delete.className == "")) {
+  if (((text && iniciative) !== "") && (block == 0)) {
     var newItem = document.createElement("li")
     var iniValue = document.createElement("input")
 
@@ -46,6 +53,21 @@ function sortItems() {
   }
 }
 
+function createEffect() {
+  var target = event.target
+  var tN = turnNumber.value;
+  var dN = diceNumber.value;
+  var selectedOption = opcoes.options[opcoes.selectedIndex].text;
+
+  if (tN !== "" && dN !== "") {
+    var newSpan = document.createElement("span");
+    newSpan.textContent = selectedOption + "RT: " + tN + ", DICE: " + dN;
+    target.appendChild(newSpan);
+
+    turnNumber.value = "";
+    diceNumber.value = "";
+  }
+}
 
 
 button.addEventListener("click", function () {
@@ -68,10 +90,23 @@ myTextBox.addEventListener("keydown", function (event) {
   }
 });
 
+//SELECT ITEM FOR EFFECT
+selectElement.addEventListener("click", function (){
+  if(selectElement.className == "slct"){
+    selectElement.classList = ""
+    block = 0
+  }
+  else{
+  selectElement.classList = "slct"
+  block = 1
+  }
+})
+
+
 //NEXT TURN
 nextTurn.addEventListener("click", function () {
   var save = myList.firstElementChild
-  if(save && (Delete.className !== "dm") && myList.getElementsByTagName("li").length > 1){
+  if(save && (block == 0) && myList.getElementsByTagName("li").length > 1){
   myList.removeChild.firstElementChild
   myList.append(save)
   }
@@ -84,7 +119,7 @@ nextTurn.addEventListener("click", function () {
 })
 //PREVIOUS TURN
 previousTurn.addEventListener("click", function () {
-  if(Delete.className == "dm" || myList.getElementsByTagName("li").length <= 1){
+  if(block == 1 || myList.getElementsByTagName("li").length <= 1){
     previousTurn.classList = "shakeElement"
     previousTurn.addEventListener("animationend", () => {
       previousTurn.classList = ""
@@ -96,7 +131,7 @@ previousTurn.addEventListener("click", function () {
 })
 //AUTOMATIC ORDER
 refresh.addEventListener("click", function () {
-  if(Delete.className == "dm" || myList.getElementsByTagName("li").length <= 1){
+  if(block == 1 || myList.getElementsByTagName("li").length <= 1){
     refresh.classList = "shakeElement"
     refresh.addEventListener("animationend", () => {
       refresh.classList = ""
@@ -111,23 +146,36 @@ refresh.addEventListener("click", function () {
 Delete.addEventListener("click", function (){
   if(Delete.classList == "dm"){
     Delete.classList = ""
+    block = 0
   }
   else{
   Delete.classList = "dm"
+  block = 1
   }
 })
+
 myList.addEventListener("click", function() {
   var target = event.target
   if(Delete.classList == "dm"){
     myList.removeChild(target)
   }
+
+  if(selectElement.className == "slct"){
+    createEffect()
+    selectElement.classList = ""
+    block = 0
+}
 })
 myList.addEventListener("mouseover", function() {
   var target = event.target
   if(Delete.classList == "dm"){
-    target.classList = "dm"
+    target.classList = "deleteAnim"
+  }
+  if(selectElement.className == "slct"){
+    target.classList = "slctAnim"
   }
   myList.addEventListener("mouseout", function() {
+    target.classList = ""
     target.classList = ""
   })
   
@@ -136,11 +184,27 @@ myList.addEventListener("mouseover", function() {
 //CHANGE INPUT VALUE
 myList.addEventListener("input", function (event) {
   var target = event.target;
-  if (target.tagName === "INPUT") {
+  if (target.tagName === "INPUT" && block == 0) {
     var listItem = target.closest("li");
     if (listItem) {
       listItem.id = target.value;
       listItem.title = target.value;
     }
   }
+});
+
+//OPEN TAB
+document.addEventListener("DOMContentLoaded", function () {
+  const tabButtons = document.querySelectorAll(".tab-button");
+
+  tabButtons.forEach(function (span) {
+    span.addEventListener("click", function () {
+      const content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  });
 });
